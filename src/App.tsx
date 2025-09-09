d61a41bb36243533973b26db9bc05f5321da8d0c
Aditi17-art <dhariwaladiti17@gmail.com>
2025-09-09 17:04:01 +0530

    contet

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Products from "./pages/Products";
-import Gallery from "./pages/Gallery";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";
+import Home from "./hooks/pages/Home";
+import About from "./hooks/pages/About";
+import Products from "./hooks/pages/Products";
+import Gallery from "./hooks/pages/Gallery";
+import Contact from "./hooks/pages/Contact";
+import NotFound from "./hooks/pages/NotFound";
 
 const queryClient = new QueryClient();
 
